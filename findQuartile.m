091b7cbabeee06502c6891a57c8ef3146ff10b9e
harimanask <harimanaskaruman@gmail.com>
2019-09-14 21:23:48 -0400

    Add files via upload

---
+++ findQuartile.m
@@ -0,0 +1,7 @@
+testfiledir = 'C:\Users\sambhu\GTFiles\';
+filenames = dir(fullfile(testfiledir, '*.txt'));
+Vel3Q = zeros(1,numel(filenames));
+for k =1:numel(filenames)
+    inputfile = filenames(k).name;
+    Vel3Q(k)= motiondynamics(inputfile);
+end
