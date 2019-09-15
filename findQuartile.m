ebfeb6eaed03b7de0b24849eaba6ce2d2618986e
harimanask <harimanaskaruman@gmail.com>
2019-09-14 21:22:55 -0400

    Delete findQuartile.m

--- findQuartile.m
+++
@@ -1,7 +0,0 @@
-testfiledir = 'C:\Users\rkkaruman\GTFiles\';
-filenames = dir(fullfile(testfiledir, '*.txt'));
-Vel3Q = zeros(1,numel(filenames));
-for k =1:numel(filenames)
-    inputfile = filenames(k).name;
-    Vel3Q(k)= motiondynamics(inputfile);
-end
